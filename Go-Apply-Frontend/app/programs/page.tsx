7d5826605be577ce4f5f4d6dfddbecb03b1f2489
Tejas  Jahagirdar <jahagirdartejas181@gmail.com>
2025-11-15 23:50:46 +0530

    Auth context error fixed help and support fix,program api fixed

--- Go-Apply-Frontend/app/programs/page.tsx
+++ Go-Apply-Frontend/app/programs/page.tsx
@@ -94,15 +94,17 @@ const [selectedProgram, setSelectedProgram] = useState<University | null>(null);
 
 
   useEffect(() => {
+    debugger
     let mounted = true;
     const fetchPrograms = async () => {
       try {
+        debugger
         setLoadingPrograms(true);
         // call backend; you can pass query params here if needed
         const res = await axios.get<{
           success: boolean;
           data: { programs: Program[] };
-        }>("{API_URL}/api/search/programs");
+        }>(`${API_URL}/search/programs`);
 
         if (!mounted) return;
 
